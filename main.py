bbc49068cbe3a8a37145d7d10e15fc98c85cbcd7
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-29 11:54:18 +0545

    independent variable is encoded as the combination of 0's and 1's

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 #importing the dataset
 df = pd.read_csv('Data.csv')
@@ -12,4 +14,6 @@ imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
 imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
 
-print(x[:,1:3])
+#encoding independent variable
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+x = np.array(ct.fit_transform(x))
