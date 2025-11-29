2a1d38562a00d1d59a1cde184c7cbb6074e04fb6
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-29 11:57:45 +0545

    test set and training set are splited

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder , LabelEncoder
+from sklearn.model_selection import train_test_split
 
 #importing the dataset
 df = pd.read_csv('Data.csv')
@@ -22,3 +23,6 @@ x = np.array(ct.fit_transform(x))
 le = LabelEncoder()
 y = le.fit_transform(y)
 print(y)
+
+#creating training set and test set
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=1)
