0d392ed68b3761293544927f08546badf8ddaa06
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-29 11:56:00 +0545

    independent variable is encoded

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder , LabelEncoder
 
 #importing the dataset
 df = pd.read_csv('Data.csv')
@@ -17,3 +17,8 @@ x[:,1:3] = imputer.transform(x[:,1:3])
 #encoding independent variable
 ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
 x = np.array(ct.fit_transform(x))
+
+#encoding dependent variable
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
