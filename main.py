300d323d2cdba1989d7acbebec602404c2c0b866
Anish Sapkota <anishsapkota926@gmail.com>
2025-11-29 11:50:21 +0545

    missing data is replaced by the mean of the respective the column

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 #importing the dataset
 df = pd.read_csv('Data.csv')
 x = df.iloc[:,:-1].values
 y = df.iloc[:,-1].values
 
-print(x)
-print(y)
+#taking care of missing data
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(x[:,1:3])
+x[:,1:3] = imputer.transform(x[:,1:3])
+
+print(x[:,1:3])
